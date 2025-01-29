575614e97cf303b83d16fceeefbb6e3dc7469298
alex <aleksejkosarevskij@gmail.com>
2025-01-29 20:07:43 +0100

    Moved button creating to a different function

--- frameworks_drivers/rendering/pyglet_app.py
+++ frameworks_drivers/rendering/pyglet_app.py
@@ -14,6 +14,54 @@ from interface_adapters.pil_to_pyglet_image import pil_to_pyglet_image
 from use_cases.gui.layout_manager import LayoutGetter
 
 
+def get_button(text: str, w_px: int, h_px: int, font_size: int, unpressed_font: str, hover_font: str, pressed_font: str,
+               but_x: int, but_y: int, batch: pyglet.graphics.Batch):
+    unpressed = generate_image(
+        text=text,
+        font_size=font_size,
+        width=w_px,
+        height=h_px,
+        font_name=unpressed_font,
+        font_color="black",
+        background_color="white"
+    )
+    hover = generate_image(
+        text=text,
+        font_size=font_size,
+        width=w_px,
+        height=h_px,
+        font_name=hover_font,
+        font_color="black",
+        background_color="white"
+    )
+    pressed = generate_image(
+        text=text,
+        font_size=font_size,
+        width=w_px,
+        height=h_px,
+        font_name=pressed_font,
+        font_color="black",
+        background_color="lightgray"
+    )
+
+    button = pyglet.gui.PushButton(
+        x=but_x,
+        y=but_y,
+        pressed=pil_to_pyglet_image(pressed),
+        unpressed=pil_to_pyglet_image(unpressed),
+        hover=pil_to_pyglet_image(hover),
+        batch=batch
+    )
+
+    # h_px = procent_to_px(item["height"], self.window.height)
+    # os.path.join(font_path, item["text-font"])
+    #
+    # button
+    # x=procent_to_px(pos["x"], self.window.width) - (w_px // 2),
+
+    return button
+
+
 class PygletApp:
     def __init__(self):
         self.window = pyglet.window.Window(width=800, height=600, resizable=True)
@@ -66,41 +114,18 @@ class PygletApp:
                     self.all_labels.append(label)
                 elif item["type"] == "press-button":
                     font_path = "resources"
-                    unpressed = generate_image(
-                        text=item["text"],
-                        font_size=procent_to_px(item["text-size"], self.window.height),
-                        width=procent_to_px(item["width"], self.window.width),
-                        height=procent_to_px(item["height"], self.window.height),
-                        font_name=os.path.join(font_path, item["text-font"]),
-                        font_color="black",
-                        background_color="white"
-                    )
-                    hover = generate_image(
-                        text=item["text"],
-                        font_size=procent_to_px(item["text-size"], self.window.height),
-                        width=procent_to_px(item["width"], self.window.width),
-                        height=procent_to_px(item["height"], self.window.height),
-                        font_name=os.path.join(font_path, item["text-font"] + "_Bold"),
-                        font_color="black",
-                        background_color="white"
-                    )
-                    pressed = generate_image(
-                        text=item["text"],
-                        font_size=procent_to_px(item["text-size"], self.window.height),
-                        width=procent_to_px(item["width"], self.window.width),
-                        height=procent_to_px(item["height"], self.window.height),
-                        font_name=os.path.join(font_path, item["text-font"] + "_Bold"),
-                        font_color="black",
-                        background_color="lightgray"
-                    )
 
-                    button = pyglet.gui.PushButton(
-                        x=procent_to_px(pos["x"], self.window.width) - (procent_to_px(item["width"], self.window.width)//2),
-                        y=procent_to_px(pos["y"], self.window.height) - (procent_to_px(item["height"], self.window.height)//2),
-                        pressed=pil_to_pyglet_image(pressed),
-                        unpressed=pil_to_pyglet_image(unpressed),
-                        hover=pil_to_pyglet_image(hover),
-                        batch=self.gui_batch
+                    button = get_button(
+                        item["text"],
+                        procent_to_px(item["width"], self.window.width),
+                        procent_to_px(item["height"], self.window.height),
+                        procent_to_px(item["text-size"], self.window.height),
+                        os.path.join(font_path, "Arial.ttf"),
+                        os.path.join(font_path, "Arial_Bold.ttf"),
+                        os.path.join(font_path, "Arial_Bold.ttf"),
+                        procent_to_px(pos["x"], self.window.width) - (procent_to_px(item["width"], self.window.width) // 2),
+                        procent_to_px(pos["y"], self.window.height) - (procent_to_px(item["height"], self.window.height) // 2),
+                        self.gui_batch
                     )
 
                     self.window.push_handlers(button)
