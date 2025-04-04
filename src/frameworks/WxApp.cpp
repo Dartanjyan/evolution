7d37076733a725ee48950c0f90b87c10d202115f
alex <aleksejkosarevskij@gmail.com>
2025-04-04 17:57:51 +0200

    Added print.cpp/h for testing cmake building

--- src/frameworks/WxApp.cpp
+++ src/frameworks/WxApp.cpp
@@ -1,8 +1,10 @@
 #include "WxApp.h"
 #include "WxFrame.h"
+#include "../use_cases/print.h"
 
 bool WxApp::OnInit() {
     WxFrame *frame = new WxFrame();
     frame->Show(true);
+    print("WxApp::OnInit: Frame shown");
     return true;
 }
---
+++ src/use_cases/print.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "print.h"
+
+// comment
+void print(const char* message) {
+    std::cout << message << std::endl;
+}
