2d4a6924110c128d13861c47a87c7dd1f37436b3
alex <aleksejkosarevskij@gmail.com>
2025-05-02 23:01:16 +0200

    Working script and linking locally build wxwidgets

--- scripts/build_wx.sh
+++ scripts/build_wx.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 set -e
 
-WX_DIR="$(realpath $(dirname $0))/../3rd_party/wxWidgets"
+WX_DIR="$(realpath $(realpath $(dirname $0))/../3rd_party/wxWidgets)"
 SRC_DIR="${WX_DIR}/src"
 INSTALL_DIR="${WX_DIR}/install"
 
 mkdir -p "${SRC_DIR}"
-git clone https://github.com/wxWidgets/wxWidgets.git "${SRC_DIR}/wxWidgets_external"
-cd "${SRC_DIR}/wxWidgets_external"
-git checkout v3.2.6
+git clone https://github.com/wxWidgets/wxWidgets.git "${SRC_DIR}"
+git config --global --add safe.directory ${SRC_DIR}
+cd "${SRC_DIR}"
+git checkout v3.2.8
 git submodule update --init 3rdparty/*
 
 cmake -B build \
     --fresh \
-    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
     -DwxBUILD_CMAKE_SUPPORT=ON \
     -DwxBUILD_SHARED=ON \
     -DwxBUILD_TESTS=OFF \
-    -DwxBUILD_EXAMPLES=OFF \
-    -DwxBUILD_COMPONENTS="core;base"
-cmake --build build --config Release
+    -DwxBUILD_EXAMPLES=OFF
+cmake --build build
 cmake --install build
 rm -rf ${SRC_DIR}
 
